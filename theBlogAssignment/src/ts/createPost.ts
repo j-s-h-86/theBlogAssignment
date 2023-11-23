e9a79d3c62faffb5e5b6de8983c52503cd9a0328
THEOD0R1 <squirrelnub@gmail.com>
2023-11-23 13:53:01 +0100

    Completed new add post function

--- theBlogAssignment/src/ts/createPost.ts
+++ theBlogAssignment/src/ts/createPost.ts
@@ -8,24 +8,48 @@ let postList = [blueSky, redSky];
 const postsContainer = document.getElementById("blogPosts");
 
 export const createHtml = () => {
+  if (postsContainer) {
+    postsContainer.innerHTML = "";
+  }
+
   for (let i = 0; i < postList.length; i++) {
     const postContainer = document.createElement("article");
     postsContainer?.appendChild(postContainer);
     console.log("Hej");
-    
-    const title = document.createElement('h3');
+
+    const title = document.createElement("h3");
     title.innerHTML = postList[i].title;
     postContainer.appendChild(title);
 
-    const description = document.createElement('span');
+    const description = document.createElement("span");
     description.innerHTML = postList[i].blogContent;
     postContainer.appendChild(description);
 
-    const addFavorite = document.createElement('button');
-    addFavorite.innerHTML = 'Favorit';
+    const addFavorite = document.createElement("button");
+    addFavorite.innerHTML = "Favorit";
     postContainer.appendChild(addFavorite);
-
   }
 };
 
+document.getElementById("createPostForm")?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addNewPost();
+});
 
+const addNewPost = () => {
+  const userTitleInput = (
+    document.getElementById("titleInput") as HTMLInputElement
+  ).value;
+
+  const userBlogContent = (
+    document.getElementById("blogContent") as HTMLTextAreaElement
+  ).value;
+
+  (document.getElementById("titleInput") as HTMLInputElement).value = "";
+
+  (document.getElementById("blogContent") as HTMLTextAreaElement).value = "";
+
+  const newPost = new Posts(`${userTitleInput}`, `${userBlogContent}`);
+  postList.push(newPost);
+  createHtml();
+};
