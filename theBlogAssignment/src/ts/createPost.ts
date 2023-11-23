537074ecd8152ba5d44ade00ca5f889fddedb7aa
johan.hammarstedt@j-s-h.com <johan.hammarstedt@j-s-h.com>
2023-11-23 11:16:14 +0100

    completed createHTML

--- theBlogAssignment/src/ts/createPost.ts
+++ theBlogAssignment/src/ts/createPost.ts
@@ -11,11 +11,20 @@ export const createHtml = () => {
   for (let i = 0; i < postList.length; i++) {
     const postContainer = document.createElement("article");
     postsContainer?.appendChild(postContainer);
+    console.log("Hej");
     
     const title = document.createElement('h3');
+    title.innerHTML = postList[i].title;
+    postContainer.appendChild(title);
+
     const description = document.createElement('span');
+    description.innerHTML = postList[i].blogContent;
+    postContainer.appendChild(description);
+
     const addFavorite = document.createElement('button');
     addFavorite.innerHTML = 'Favorit';
+    postContainer.appendChild(addFavorite);
+
   }
 };
 
--- theBlogAssignment/src/ts/main.ts
+++ theBlogAssignment/src/ts/main.ts
@@ -1,1 +1,4 @@
 import "./../scss/style.scss";
+import { createHtml } from "./createPost";
+
+createHtml();
